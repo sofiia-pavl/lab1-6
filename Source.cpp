12ccae1676231c25eb33dbbbb231c4a7776a6912
sofiia-pavl <72608224+sofiia-pavl@users.noreply.github.com>
2021-03-02 08:42:28 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,17 +7,14 @@ int main()
 {
 	Vector3d q;
 	q.Read();
-	q.Display();
 	cout << "edding vector:" << endl;
 	q.addV();
 	q.Display();
 
 	Vector3d f;
 	f.Read();
-	f.Display();
 	cout << "multiplay to vector:" << endl;
 	f.multiplayV();
-	f.Display();
 
 	return 0;
 }
